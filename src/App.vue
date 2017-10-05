65de913369940df7c890944bbdafb739b5f7f9cf
Konstantin <2447785@gmail.com>
2017-10-05 22:07:24 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <form id="signup-form" method="post" action="#">
+    <input type="email" name="email" id="email" placeholder="Email" />
+    <input type="submit" value="Подписаться" />
+  </form>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
